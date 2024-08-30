a69409f504f6ab1398c1710d06c62c67f079c6bf
Leandro Garzon <hekoej@gmail.com>
2024-08-30 15:32:18 -0500

    UI kitten- Componentes estilizados

---
+++ src/ProductsApp.tsx
@@ -0,0 +1,24 @@
+import './gesture-handler';
+import { NavigationContainer } from "@react-navigation/native";
+import { StackNavigator } from './presentation/navigation/StackNavigator';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
+import { useColorScheme } from 'react-native';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+
+export const ProductsApp = () => {
+
+  const colorScheme = useColorScheme();
+  const theme = colorScheme === 'dark' ? eva.dark : eva.light;
+
+  return (
+    <>
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider {...eva} theme={theme}>
+        <NavigationContainer>
+          <StackNavigator />
+        </NavigationContainer>
+      </ApplicationProvider>
+    </>
+  )
+}
---
+++ src/presentation/screens/loading/LoadinScreen.tsx
@@ -0,0 +1,9 @@
+import { Layout, Text } from "@ui-kitten/components"
+
+export const LoadingScreen = () => {
+  return (
+    <Layout>
+      <Text>LoadingScreen</Text>
+    </Layout>
+  )
+}
