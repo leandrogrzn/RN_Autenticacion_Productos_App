3d9a8744819858d5357dedf3fc4bd8cbbf589aa0
Leandro Garzon <hekoej@gmail.com>
2024-09-07 21:04:08 -0500

    Pantalla de Producto

--- src/presentation/screens/product/ProductScreen.tsx
+++ src/presentation/screens/product/ProductScreen.tsx
@@ -1,9 +1,33 @@
-import { Text, View } from "react-native";
+import { Text } from "@ui-kitten/components"
+import { MainLayout } from "../../layouts/MainLayout"
+import { StackScreenProps } from "@react-navigation/stack"
+import { RootStackParams } from "../../navigation/StackNavigator"
+import { useQuery } from "@tanstack/react-query"
+import { getProductById } from "../../../actions/products/get-product-by-id"
+import { useRef } from "react"
 
-export const ProductScreen = () => {
+interface Props extends StackScreenProps<RootStackParams, 'ProductScreen'>{}
+
+export const ProductScreen = ({route}:Props) => {
+
+  const productIdRef = useRef(route.params.productId)
+
+  const { data: product } = useQuery({
+    queryKey: ['product', productIdRef.current],
+    queryFn: () => getProductById(productIdRef.current),
+  })
+
+  
+  if(!product) {
+    return (<MainLayout title="Cargando..."/>)
+  }
+
   return (
-    <View>
-      <Text>ProductScreen</Text>
-    </View>
+    <MainLayout
+      title={product.title}
+      subTitle={`Precio: ${product.price}`}
+    >
+      <Text>hola mundo</Text>
+    </MainLayout>
   )
 }
